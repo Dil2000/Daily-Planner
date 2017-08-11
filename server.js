692cd43d1dcd5a9fa02302956b10c16168221999
Dil2000 <dnks2000@gmail.com>
2017-08-11 19:44:13 -0400

    all

--- server.js
+++ server.js
@@ -27,13 +27,14 @@ app.set("view engine", "handlebars");
 
 var connection;
 if (process.env.JAWSDB_URL){
-  connection.mysql.createConnection(process.eng.JAWSDB_URL);
+  connection = mysql.createConnection(process.eng.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: " vkepj7bxyushec62",
-    password: "z6ipautgk863ehfi",
-    database: 'day_planner_db'  
+    host: 'mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'vkepj7bxyushec62',
+    password: 'z6ipautgk863ehfi',
+    database: 'day_planner_db',
+    port : '3306'
   });
 };
 
